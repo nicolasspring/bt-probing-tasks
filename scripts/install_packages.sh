2f94f3e01ba9fba3fce05ae437d64c8c687d87da
Nicolas Spring <nispri@login0>
2020-03-07 18:34:35 +0100

    install specific torch version

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -4,7 +4,12 @@ SCRIPTS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO=`dirname "$SCRIPTS"`
 
 # needed for model training
-pip install torch fastBPE sacremoses subword_nmt
+pip install fastBPE sacremoses subword_nmt
+
+# specific torch version
+wget https://download.pytorch.org/whl/cu100/torch-1.3.0%2Bcu100-cp36-cp36m-linux_x86_64.whl
+pip install torch-1.3.0+cu100-cp36-cp36m-linux_x86_64.whl
+rm torch-1.3.0+cu100-cp36-cp36m-linux_x86_64.whl
 
 # needed for probing tasks
 pip install numpy scipy scikit-learn matplotlib pandas

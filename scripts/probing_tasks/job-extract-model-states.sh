07d02ac3abd249d5b0ad73a0785340557e455602
nicolasspring <springnicolas@hispeed.ch>
2020-03-12 18:27:05 +0100

    changed outpaths for model states

--- scripts/probing_tasks/job-extract-model-states.sh
+++ scripts/probing_tasks/job-extract-model-states.sh
@@ -26,27 +26,27 @@ BINOISED=$REPO/model_states/en_de_parallel_plus_bt_noised
 # task 1: bitext vs beam back-translation (beamBT)
 
 fairseq-states $BITEXT/train.en --path $CHECKPOINT_DIR_BEAM/checkpoint.avg10.pt \
-    --states-dir $BIBEAM/bitext/train.en
+    --states-dir $BIBEAM/bitext/train
 
 fairseq-states $BITEXT/test.en --path $CHECKPOINT_DIR_BEAM/checkpoint.avg10.pt \
-    --states-dir $BIBEAM/bitext/test.en
+    --states-dir $BIBEAM/bitext/test
 
 fairseq-states $BEAM/train.en --path $CHECKPOINT_DIR_BEAM/checkpoint.avg10.pt \
-    --states-dir $BIBEAM/beam/train.en
+    --states-dir $BIBEAM/beam/train
 
 fairseq-states $BEAM/test.en --path $CHECKPOINT_DIR_BEAM/checkpoint.avg10.pt \
-    --states-dir $BIBEAM/beam/test.en
+    --states-dir $BIBEAM/beam/test
 
 # task 2: bitext vs noised back-translation (noisedBT)
 
 fairseq-states $BITEXT/train.en --path $CHECKPOINT_DIR_NOISED/checkpoint.avg10.pt \
-    --states-dir $BINOISED/bitext/train.en
+    --states-dir $BINOISED/bitext/train
 
 fairseq-states $BITEXT/test.en --path $CHECKPOINT_DIR_NOISED/checkpoint.avg10.pt \
-    --states-dir $BINOISED/bitext/test.en
+    --states-dir $BINOISED/bitext/test
 
 fairseq-states $NOISED/train.en --path $CHECKPOINT_DIR_NOISED/checkpoint.avg10.pt \
-    --states-dir $BINOISED/noised/train.en
+    --states-dir $BINOISED/noised/train
 
 fairseq-states $NOISED/test.en --path $CHECKPOINT_DIR_NOISED/checkpoint.avg10.pt \
-    --states-dir $BINOISED/noised/test.en
+    --states-dir $BINOISED/noised/test

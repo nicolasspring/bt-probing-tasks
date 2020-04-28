65d53991c1aa5a3ff45639cb1971abb2402747dc
nicolasspring <springnicolas@hispeed.ch>
2020-04-28 11:16:45 +0200

    calculate tokenized and detokenized BLEU

--- scripts/evaluation/job-evaluate-beam-model.sh
+++ scripts/evaluation/job-evaluate-beam-model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=02:00:00
+#SBATCH --time=00:30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:Tesla-V100:1
@@ -17,6 +17,15 @@ cd $REPO
 
 CHECKPOINT_DIR=$REPO/checkpoints/checkpoints_en_de_parallel_plus_bt_beam
 
+echo "Calculating tokenized BLEU on newstest2017..."
+bash $REPO/software/fairseq-states/examples/backtranslation/tokenized_bleu.sh \
+    wmt17 \
+    en-de \
+    $REPO/data-bin/wmt18_en_de \
+    $REPO/data-bin/wmt18_en_de/code \
+    $CHECKPOINT_DIR/checkpoint.avg10.pt
+
+echo "Calculating (detokenized) sacrebleu on newstest2017..."
 bash $REPO/software/fairseq-states/examples/backtranslation/sacrebleu.sh \
     wmt17 \
     en-de \
--- scripts/evaluation/job-evaluate-reverse-model.sh
+++ scripts/evaluation/job-evaluate-reverse-model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=02:00:00
+#SBATCH --time=00:30:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --gres=gpu:Tesla-V100:1
@@ -17,6 +17,15 @@ cd $REPO
 
 CHECKPOINT_DIR=$REPO/checkpoints/checkpoints_de_en_parallel
 
+echo "Calculating tokenized BLEU on newstest2017..."
+bash $REPO/software/fairseq-states/examples/backtranslation/tokenized_bleu.sh \
+    wmt17 \
+    de-en \
+    $REPO/data-bin/wmt18_en_de \
+    $REPO/data-bin/wmt18_en_de/code \
+    $CHECKPOINT_DIR/checkpoint_best.pt
+
+echo "Calculating (detokenized) sacrebleu on newstest2017..."
 bash $REPO/software/fairseq-states/examples/backtranslation/sacrebleu.sh \
     wmt17 \
     de-en \
